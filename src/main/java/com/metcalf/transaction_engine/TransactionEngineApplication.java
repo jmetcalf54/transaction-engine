108913cb9fe8f603cf82cd6e98d6bcd3fe335aeb
C:/Users/Joseph Metcalf <josephmetcalf540@gmail.com>
2026-02-17 23:36:46 -0600

    Initial Spring Boot project setup

---
+++ src/main/java/com/metcalf/transaction_engine/TransactionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.metcalf.transaction_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionEngineApplication.class, args);
+	}
+
+}
